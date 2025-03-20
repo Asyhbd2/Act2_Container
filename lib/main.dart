868638c573f446eadadee34bb3db4d60eaa7b9d5
ANGEL_TADEO_DE_LEON_CENICEROS <a.22308051281165@cbtis128.edu.mx>
2025-03-20 18:59:37 +0000

    Pushear cambios 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiContainer());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiContainer extends StatelessWidget {
+  const MiContainer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    // Lista de datos
+    List<String> datos = ["Flutter", "Dart", "Widget", "Container", "Row"];
+
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("De Leon Container"),
+          titleTextStyle: const TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          centerTitle: true,
+          backgroundColor: const Color(0xffecdf2a),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center, // Centrar la Row
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16),
+                width: 200, // Ancho del container
+                height: 210, // Altura del container
+                decoration: BoxDecoration(
+                  color: Color(0xffc8ffa8), // Color suave
+                  borderRadius: BorderRadius.circular(15), // Bordes redondeados
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 10,
+                      spreadRadius: 10,
+                      offset: const Offset(4, 4),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment:
+                      MainAxisAlignment.center, // Centrar verticalmente
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // Centrar horizontalmente
+                  children: datos
+                      .map((dato) => Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 4.0),
+                            child: Text(
+                              dato,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
